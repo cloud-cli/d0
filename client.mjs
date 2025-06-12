48c4de629f5fcba6e5a6c3e8c4ae918671ba9210
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2025-06-12 15:31:03 +0200

    chore: add get(), run() and all() to client library

--- client.mjs
+++ client.mjs
@@ -1,20 +1,32 @@
-const baseURL = 'https://__API_URL__';
+const baseURL = "https://__API_URL__";
 
-export default {
-  async query(statement, data, method = 'run') {
-    const req = await fetch(new URL('/query', baseURL), {
-      method: 'POST',
-      body: JSON.stringify({
-        s: statement,
-        d: data || null,
-        m: method,
-      }),
-    });
+export async function query(statement, data, method = "run") {
+  const req = await fetch(new URL("/query", baseURL), {
+    method: "POST",
+    body: JSON.stringify({
+      s: statement,
+      d: data || null,
+      m: method,
+    }),
+  });
 
-    if (req.ok) {
-      return await req.json();
-    }
+  if (req.ok) {
+    return await req.json();
+  }
 
-    throw new Error(await req.text());
-  },
-};
+  throw new Error(await req.text());
+}
+
+export function get(statement, data) {
+  return query(statement, data, "get");
+}
+
+export function run(statement, data) {
+  return query(statement, data, "run");
+}
+
+export function all(statement, data) {
+  return query(statement, data, "all");
+}
+
+export default { query, get, run, all };
